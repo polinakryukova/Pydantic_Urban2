a395e2583c624e660ae176d8143b7cebc171da72
polinakryukova <polinakpa@gmail.com>
2024-11-08 10:59:18 +0300

    2nd commit

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-from task import Task
-from user import User
+from .task import Task
+from .user import User
 
--- app/models/task.py
+++ app/models/task.py
@@ -1,7 +1,7 @@
 from app.backend.db import Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
-import user
+from app.models import user
 
 class Task(Base):
     __tablename__='tasks'
